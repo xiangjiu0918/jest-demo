d7302ae2d41a04e513045dea209d7a02ef84350a
xiangjiu0918 <11614542+ajjj0918@user.noreply.gitee.com>
2025-01-04 21:15:59 +0800

    complete react16 function component with react-testing-library

--- src/container/TodoList/__tests__/unit/TodoList.js
+++ src/container/TodoList/__tests__/unit/TodoList.js
@@ -16,14 +16,29 @@ const testFunc = (undo, add, del) => {
   deleteUndoItem = del;
 }
 
-beforeEach(() => {
-  render(<TodoList testFunc={testFunc} />);
-})
 
 test('快照测试', () => {
+  render(<TodoList testFunc={testFunc} />);
   expect(screen).toMatchSnapshot();
 })
 
 test('state.undoList初始化为空', () => {
+  render(<TodoList testFunc={testFunc} />);
   expect(undoListData).toEqual([]);
+})
+
+test('当调用addUndoItem方法时，更新state.undoList', () => {
+  render(<TodoList testFunc={testFunc} />);
+  const inputData = 'world';
+  addUndoItem(inputData);
+  expect(changeUndoList).toHaveBeenLastCalledWith([inputData]);
+})
+
+
+test('当调用deleteUndoItem方法时，更新state.undoList', () => {
+  const undoListData = ['hello', 'world'];
+  useStateMock.mockImplementation(init => [undoListData, changeUndoList]);
+  render(<TodoList testFunc={testFunc} />);
+  deleteUndoItem(0);
+  expect(changeUndoList).toHaveBeenLastCalledWith([undoListData[1]]);
 })
